eb5c0f5b18383032c0b79367e7e8d31caaa60075
Im-Erik <erik.orrego.n@gmail.com>
2025-04-21 12:13:43 -0400

    ajuste responsivo a pagina

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { CarruselIconsComponent } from "./components/carrusel-icons/carrusel-icons.component";
 import ContactFormComponent from './components/contact-form/contact-form.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CarruselIconsComponent, ContactFormComponent],
+  imports: [CarruselIconsComponent, ContactFormComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
